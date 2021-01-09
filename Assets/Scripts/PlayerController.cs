344e507ec5a12f108c122c5641bf59d4bfc975b0
Hifumi123 <you@example.com>
2021-01-09 21:51:11 +0800

    完成项目，正式提交

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public MeshCollider ground;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider == ground)
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
